bf1111b8c44be2a598f178fa97e7e391b8381027
German <gerechegaray@gmail.com>
2025-07-30 15:39:21 -0300

    Render 1.1

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': 'https://backend-react-1kcz.onrender.com'
+      '/api': 'https://cobros-dcg.onrender.com'
     }
   }
 });
