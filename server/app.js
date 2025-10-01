7ff95305226f797ca1dcd7c377fa625554c436d2
German <gerechegaray@gmail.com>
2025-10-01 11:38:47 -0300

    Fix: Agregar Cache-Control a headers CORS permitidos
    - Solucionar error CORS en producciÃ³n
    - Permitir header Cache-Control en requests
    - Error: Request header field cache-control is not allowed

--- server/app.js
+++ server/app.js
@@ -170,7 +170,7 @@ app.use(cors({
   ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization']
+  allowedHeaders: ['Content-Type', 'Authorization', 'Cache-Control']
 }));
 
 app.use(express.json());
