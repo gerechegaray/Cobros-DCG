de319a8dff2918de9bd92aeac44c9ae550a6de6e
German <gerechegaray@gmail.com>
2025-10-01 11:42:00 -0300

    Fix: Agregar Pragma a headers CORS permitidos
    - Solucionar segundo error CORS en producciÃ³n
    - Permitir header Pragma en requests
    - Error: Request header field pragma is not allowed

--- server/app.js
+++ server/app.js
@@ -170,7 +170,7 @@ app.use(cors({
   ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization', 'Cache-Control']
+  allowedHeaders: ['Content-Type', 'Authorization', 'Cache-Control', 'Pragma']
 }));
 
 app.use(express.json());
